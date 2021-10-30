518dadfde98d811e5cbfd67f90e5c389d63e7f56
Noah Citron <noah@jeff.org>
2021-10-30 02:13:22 -0400

    add ape together contracts

---
+++ src/ape-together/ApeRebalanceExtension.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+pragma experimental ABIEncoderV2;
+
+import { GIMExtension } from "indexcoop/contracts/adapters/GIMExtension.sol";
+import { IBaseManager } from "indexcoop/contracts/interfaces/IBaseManager.sol";
+import { IGeneralIndexModule } from "indexcoop/contracts/interfaces/IGeneralIndexModule.sol";
+
+import { OwlNFT } from "./OwlNFT.sol";
+
+contract ApeRebalanceExtension is GIMExtension {
+
+    uint256 public epochLength;
+    uint256 public  currentEpochStart;
+    
+    mapping(address => uint256) public lastEpochVoted;
+    mapping(address => uint256) public votes;
+    address[] public possibleComponents;
+
+    uint256 public immutable maxComponents;
+
+    OwlNFT public owlNft;
+
+    constructor(
+        IBaseManager _manager,
+        IGeneralIndexModule _gim,
+        OwlNFT _owlNft,
+        uint256 _startTime,
+        uint256 _epochLength,
+        uint256 _maxComponents
+    )
+        public 
+        GIMExtension(_manager, _gim)
+    {
+        owlNft = _owlNft;
+        currentEpochStart = _startTime;
+        epochLength = _epochLength;
+        maxComponents = _maxComponents;
+    }
+ 
+    function vote(address[] memory _components, uint256[] memory _votes) external {
+        require(_components.length == _votes.length, "length mismatch");
+
+        if (_isEpochOver()) {
+            currentEpochStart = currentEpochStart.add(epochLength);
+
+            for (uint256 i = 0; i < possibleComponents.length; i++) {
+                votes[possibleComponents[i]] = 0;
+            }
+            delete possibleComponents;
+        }
+
+        require(lastEpochVoted[msg.sender] != currentEpochStart, "already voted");
+        lastEpochVoted[msg.sender] = currentEpochStart;
+
+        uint256 sumVotes;
+        for (uint256 i = 0; i < _components.length; i++) {
+            require(_votes[i] != 0, "no zero votes allowed");
+            if (votes[_components[i]] == 0) {
+                possibleComponents.push(_components[i]);
+            }
+            votes[_components[i]] = _votes[i];
+            sumVotes.add(_votes[i]);
+        }
+
+        require(sumVotes <= _getVotes(msg.sender), "too many votes used");
+    }
+
+    function startRebalance() external onlyOperator {
+        // forgive me father for I have sinned with this selection sort
+        address[] memory possibleLeft = possibleComponents;
+        address[] memory finalComponents = new address[](maxComponents);
+        for (uint256 i = 0; i < maxComponents; i++) {
+            uint256 max;
+            uint256 maxIndex;
+            for (uint256 j = 0; j < possibleLeft.length; j++) {
+                uint256 currentVotes = votes[possibleLeft[j]];
+                if (currentVotes > max) {
+                    max = currentVotes;
+                    maxIndex = j;
+                }
+            }
+            finalComponents[i] = possibleLeft[maxIndex];
+            (possibleLeft,) = possibleLeft.pop(maxIndex);
+        }
+
+        uint256[] memory finalUnits = new uint256[](maxComponents);
+        for (uint256 i = 0; i < maxComponents; i++) {
+            finalUnits[i] = votes[finalComponents[i]];
+        }
+
+        (
+            address[] memory newComponents,
+            uint256[] memory newComponentsTargetUnits,
+            uint256[] memory oldComponentsTargetUnits
+        ) = _sortNewAndOldComponents(finalComponents, finalUnits);
+
+        // since we fix the position multiplier to 1 we cannot have a streaming fee in any set that uses this
+        _startRebalance(newComponents, newComponentsTargetUnits, oldComponentsTargetUnits, 1 ether);
+    }
+
+    function startRebalanceWithUnits(
+        address[] memory _components,
+        uint256[] memory _targetUnits,
+        uint256 _positionMultiplier
+    )
+        external
+        override
+    {
+        revert("only democratically elected shitcoins allowed");
+    }
+
+    function _getVotes(address _voter) internal returns (uint256) {
+        uint256 bal = owlNft.balanceOf(_voter);
+        
+        uint256 totalVotes;
+        for (uint256 i = 0; i < bal; i++) {
+            uint256 id = owlNft.tokenOfOwnerByIndex(_voter, i);
+            totalVotes = totalVotes.add(owlNft.getVotes(id));
+        }
+
+        return totalVotes;
+    }
+
+    function _isEpochOver() internal returns (bool) {
+        return block.timestamp >= currentEpochStart.add(epochLength);
+    }
+}
---
+++ src/ape-together/OwlNFT.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract OwlNFT is ERC721, Ownable {
+
+    enum OwlRank { BRONZE, SILVER, GOLD }
+
+    uint256 currentId;
+    mapping(uint256 => OwlRank) ranks;
+
+    constructor() public ERC721("OwlNFT", "OWL") {}
+
+    function batchMint(address[] memory _to, OwlRank[] memory _ranks) external {
+        require(_to.length == _ranks.length, "length mismatch");
+
+        for (uint256 i = 0; i < _to.length; i++) {
+            _safeMint(_to[i], currentId);
+            ranks[currentId] = _ranks[i];
+            currentId = currentId.add(1);
+        }
+    }
+
+    function getVotes(uint256 _id) external view returns (uint256) {
+        OwlRank rank = ranks[_id];
+
+        if (rank == OwlRank.BRONZE) return 50;
+        if (rank == OwlRank.SILVER) return 75;
+        if (rank == OwlRank.GOLD) return 100;
+    }
+}
