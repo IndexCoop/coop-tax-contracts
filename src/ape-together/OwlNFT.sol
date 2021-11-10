bbbf6d12751d80aeba5c72144370b871f747c4b9
Noah Citron <noah@jeff.org>
2021-11-10 15:18:40 -0500

    use natspec (#8)

--- src/ape-together/OwlNFT.sol
+++ src/ape-together/OwlNFT.sol
@@ -4,6 +4,11 @@ pragma solidity 0.6.10;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title OwlNFT
+/// @author ncitron
+/// @notice NFT contract for tracking voting weights of Index Coop contibutors for
+/// rebalancing with the ApeRebalanceExtension. Each NFT is assinged a rank of either
+/// gold, silver, or bronze, which correlated to the number of votes assigned to the holder
 contract OwlNFT is ERC721, Ownable {
 
     enum OwlRank { BRONZE, SILVER, GOLD }
@@ -11,17 +16,14 @@ contract OwlNFT is ERC721, Ownable {
     uint256 currentId;
     mapping(uint256 => OwlRank) ranks;
 
-    /**
-     * Sets token name and symbol
-     */
+    
+    /// @notice Creates the OwlNFT
     constructor() public ERC721("OwlNFT", "OWL") {}
 
-    /**
-     * ONLY OWNER: Batch mints NFTs
-     *
-     * @param _to       array of addresses to mint to
-     * @param _ranks    owl ranks for each of the NFTs
-     */
+    /// @notice Batch mints NFTs
+    /// @dev Only callable by owner
+    /// @param _to Array of addresses to mint to
+    /// @param _ranks Owl ranks for each of the NFTs
     function batchMint(address[] memory _to, OwlRank[] memory _ranks) external onlyOwner {
         require(_to.length == _ranks.length, "length mismatch");
 
@@ -32,13 +34,9 @@ contract OwlNFT is ERC721, Ownable {
         }
     }
 
-    /**
-     * Gets the votes for the NFT. Gold owls get 100 votes, solver 75, and bronze
-     * 50.
-     *
-     * @param _id           nft id
-     * @return uint256      number of votes the id
-     */
+    /// @notice Gets the votes for the NFT. Gold owls get 100 votes, solver 75, and bronze 50.
+    /// @param _id NFT id
+    /// @return Number of votes for the id
     function getVotes(uint256 _id) external view returns (uint256) {
 
         require(_id < currentId, "invalid id");
@@ -48,7 +46,5 @@ contract OwlNFT is ERC721, Ownable {
         if (rank == OwlRank.BRONZE) return 50 ether;
         if (rank == OwlRank.SILVER) return 75 ether;
         if (rank == OwlRank.GOLD) return 100 ether;
-
-        return 0;
     }
 }
